2fd6a7e44471ac1b5461eb60fca5547fbcf0e784
Tanner-Eiland <89539118+Tanner-Eiland@users.noreply.github.com>
2023-08-10 20:54:53 -0500

    second commit

--- test.py
+++
@@ -1,9 +0,0 @@
-import imagehash
-from PIL import Image
-
-hash1 = imagehash.whash(Image.open('mtgcardImages/AdamantWillMTG.png'))
-hash2 = imagehash.whash(Image.open('mtgcardImages/MyImage1.jpg'))
-print (hash1)
-print (hash2)
-print (hash1 - hash2)
-
